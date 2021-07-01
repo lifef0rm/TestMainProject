311a97705d88d89888486e1f0b66f16d432dbf99
Michel Ferland <lifef0rm@hotmail.com>
2021-06-30 21:04:25 -0400

    modif 2

---
+++ TestLibProject1/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibProject1
+{
+    class Class2
+    {
+    }
+}
